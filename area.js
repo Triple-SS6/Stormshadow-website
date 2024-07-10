0db948cd1a42f097dd92bb53cc2f0a988e4e00f2
stormshadow6 <allanturyaguma16@gmail.com>
2024-07-10 16:15:18 +0300

    sidebar clear

---
+++ area.js
@@ -0,0 +1,20 @@
+function lengthConverterSquareFeet(valNum) {
+    var inchz = valNum*144;
+    document.querySelector('#outputSquareInch').innerHTML = `= ${inchz}`;
+    var cm = valNum*929;
+    document.querySelector('#outputSquareCentimetre').innerHTML = `= ${cm}`;
+    var millimeters = valNum*930;
+    document.querySelector('#outputSquareMillimetre').innerHTML = `= ${millimeters}`;
+    var m = valNum*0.093;
+    document.querySelector('#outputSquareMetre').innerHTML = `= ${m}`;
+}
+function lengthConverterSquareMetre(valNum) {
+    var inchz = valNum*144;
+    document.querySelector('#outputSquareInch').innerHTML = `= ${inchz}`;
+    var cm = valNum*929;
+    document.querySelector('#outputSquareCentimetre').innerHTML = `= ${cm}`;
+    var millimeters = valNum*930;
+    document.querySelector('#outputSquareMillimetre').innerHTML = `= ${millimeters}`;
+    var m = valNum*0.093;
+    document.querySelector('#outputSquareMetre').innerHTML = `= ${m}`;
+}
--- scripts.js
+++ scripts.js
@@ -5,10 +5,20 @@ function dark() {
 
 const today = new Date();
 let year = today.getFullYear();
-
 document.querySelector('.year').innerHTML = year;
-
 
 function closeD(x) {
     x.classList.toggle('change');
+    
+    sidebar1();
+}
+
+function sidebar1() {
+    var openclose = document.querySelector('.sidebar');
+    if (openclose.style.display === 'grid') {
+        openclose.style.display = 'none';
+    }
+    else {
+        openclose.style.display = 'grid';
+    }
 }
